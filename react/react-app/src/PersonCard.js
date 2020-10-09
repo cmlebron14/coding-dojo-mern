a56102e95ed30aaae6c695d8e1a448c907376726
Christina Lebron <cmlebron14@gmail.com>
2020-10-09 09:27:12 -0400

    putting it together assignment

--- react/react-app/src/PersonCard.js
+++ react/react-app/src/PersonCard.js
@@ -1,12 +1,22 @@
 import React from "react";
+import "./PersonCard.css";
 
 class PersonCard extends React.Component {
+  state = {
+    age: this.props.personAge,
+  }
+  birthday = () => {
+    let age = this.state.age;
+    age++;
+    this.setState({ age });
+  }
   render() {
     return (
       <div className="person-card">
         <h2>{this.props.personName}</h2>
-        <p>Age: {this.props.personAge}</p>
+        <p>Age: {this.state.age}</p>
         <p>Hair Color: {this.props.personHair}</p>
+        <button onClick={this.birthday}>Birthday Button</button>
       </div>
     );
   }
