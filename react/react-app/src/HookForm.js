8f0e3eb644e354e92518e74e91acc7c71a74a37f
Christina Lebron <cmlebron14@gmail.com>
2020-10-16 08:54:20 -0400

    more forms assignment

--- react/react-app/src/HookForm.js
+++ react/react-app/src/HookForm.js
@@ -7,29 +7,100 @@ const HookForm = props => {
   const [ email, setEmail ] = useState("");
   const [ password, setPassword ] = useState("");
   const [ confirmPassword, setConfirmPassword ] = useState("");
+  const [ firstnameError, setFirstnameError ] = useState("");
+  const [ lastnameError, setLastnameError ] = useState("");
+  const [ emailError, setEmailError ] = useState("");
+  const [ passwordError, setPasswordError ] = useState("");
+  const [ confirmPasswordError, setConfirmPasswordError ] = useState("");
+
+  const handleFirstname = e => {
+    setFirstname(e.target.value);
+    if (e.target.value.length < 2 && e.target.value.length > 0) {
+      setFirstnameError("First name must be at least 2 characters");
+    } else {
+      setFirstnameError("");
+    }
+  }
+  const handleLastname = e => {
+    setLastname(e.target.value);
+    if (e.target.value.length < 2 && e.target.value.length > 0) {
+      setLastnameError("Last name must be at least 2 characters");
+    } else {
+      setLastnameError("");
+    }
+  }
+  const handleEmail = e => {
+    setEmail(e.target.value);
+    if (e.target.value.length < 5 && e.target.value.length > 0) {
+      setEmailError("Email must be at least 5 characters");
+    } else {
+      setEmailError("");
+    }
+  }
+  const handlePassword= e => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 8 && e.target.value.length > 0) {
+      setPasswordError("Password must be at least 8 characters");
+    } else {
+      setPasswordError("");
+    }
+  }
+  const handleConfirmPassword = e => {
+    setConfirmPassword(e.target.value);
+    if (password !== e.target.value) {
+      setConfirmPasswordError("Password and Confirm Password must match exactly");
+    } else {
+      setConfirmPasswordError("");
+    }
+  }
 
   return (
     <div className="wrapper">
       <form>
         <div className="form-group">
           <label>First Name: </label>
-          <input type="text" onChange={ e => setFirstname(e.target.value) }/>
+          <input type="text" onChange={ handleFirstname }/>
+          {
+            firstnameError ?
+            <p className="error-message">{ firstnameError }</p> :
+            ''
+          }
         </div>
         <div className="form-group">
           <label>Last Name: </label>
-          <input type="text" onChange={ e => setLastname(e.target.value) }/>
+          <input type="text" onChange={ handleLastname }/>
+          {
+            lastnameError ?
+            <p className="error-message">{ lastnameError }</p> :
+            ''
+          }
         </div>
         <div className="form-group">
           <label>Email: </label>
-          <input type="email" onChange={ e => setEmail(e.target.value) }/>
+          <input type="email" onChange={ handleEmail }/>
+          {
+            emailError ?
+            <p className="error-message">{ emailError }</p> :
+            ''
+          }
         </div>
         <div className="form-group">
           <label>Password: </label>
-          <input type="password" onChange={ e => setPassword(e.target.value) }/>
+          <input type="password" onChange={ handlePassword }/>
+          {
+            passwordError ?
+            <p className="error-message">{ passwordError }</p> :
+            ''
+          }
         </div>
         <div className="form-group">
           <label>Confirm Password: </label>
-          <input type="password" onChange={ e => setConfirmPassword(e.target.value) }/>
+          <input type="password" onChange={ handleConfirmPassword }/>
+          {
+            confirmPasswordError ?
+            <p className="error-message">{ confirmPasswordError }</p> :
+            ''
+          }
         </div>
       </form>
       <div className="form-output">
