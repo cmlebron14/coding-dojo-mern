7ac202c2fe4a8c9ea19788b8f59fb3ca8388d991
Christina Lebron <cmlebron14@gmail.com>
2020-11-06 08:44:55 -0500

    faker api assignment

--- express/faker_api/server.js
+++ express/faker_api/server.js
@@ -1,12 +1,44 @@
 const express = require("express");
+const faker = require("faker");
+
 const app = express();
 
-// req is short for request
-// res is short for response
-app.get("/api", (req, res) => {
-  res.send("Our express api server is now sending this over to the browser");
-});
+const User = class {
+  constructor(firstName, lastName, phoneNumber, email, password) {
+    this.firstName = faker.name.firstName();
+    this.lastName = faker.name.lastName();
+    this.phoneNumber = faker.phone.phoneNumber();
+    this.email = faker.internet.email();
+    this.password = faker.internet.password();
+  }
+}
+const Company = class {
+  constructor(name, address) {
+    this.name = faker.company.companyName();
+    this.address = {
+      street: faker.address.streetAddress(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipCode: faker.address.zipCode(),
+      country: faker.address.country(),
+    }
+  }
+}
 
-const server = app.listen(8000, () =>
-  console.log(`Server is locked and loaded on port ${server.address().port}!`)
-);
+app.get("/api/users/new", (req, res) => {
+  res.send(new User());
+})
+app.get("/api/companies/new", (req, res) => {
+  res.send(new Company());
+})
+app.get("/api/user/company", (req, res) => {
+  const results = [
+    { newUser: new User() },
+    { newCompany: new Company() },
+  ]
+  return res.json({ newResults: results });
+})
+
+const server = app.listen(8000, () => {
+  console.log("Server is locked and loaded on port 8000");
+})
